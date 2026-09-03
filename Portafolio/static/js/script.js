6d8b0ba54e34cdf0f5b99322655aac1ae7656b3c
xavivope-star <xavierolmos@liceovvh.cl>
2026-09-03 17:28:40 -0400

    Portafolio 1

---
+++ Portafolio/static/js/script.js
@@ -0,0 +1,12 @@
+console.log(Conexion exitosa...)
+
+const navbar = document.querySelector('.gremlin-navbar');
+
+  window.addEventListener('scroll', () => {
+    // Si el usuario baja más de 50px, añade la clase .scrolled
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
